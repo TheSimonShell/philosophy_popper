842bc6740c2b401a9d708b60e9869a3ce67ce0a6
TheSimonShell <simao.vilas.dev@gmail.com>
2026-03-22 11:48:26 +0000

    2026-03-22 11:48

---
+++ 02 - Password aleatoria/main.py
@@ -0,0 +1,48 @@
+import string
+import random
+
+# Guarda o conjunto de caracteres disponiveis
+
+s1 = list(string.ascii_lowercase)
+s2 = list(string.ascii_uppercase)
+s3 = list(string.digits)
+s4 = list(string.punctuation)
+
+user_input = input("Quantos caracteres quer na sua password? ")
+
+while True:
+    try:
+        characters_number = int(user_input)
+
+        if characters_number < 8:
+            print("Deve ser, no mínimo, 8 caracteres.")
+            user_input = input("Insira novamente, quantos caracteres quer na sua password? ")
+
+        else:
+            break
+    except:
+        print("Por favor, insira somente o número de caracteres que quer na sua password.")
+        user_input = input("Insira novamente, quantos caracteres quer na sua password? ")
+
+random.shuffle(s1)
+random.shuffle(s2)
+random.shuffle(s3)
+random.shuffle(s4)
+
+part1 = round(characters_number * (30/100))
+part2 = round(characters_number * (20/100))
+
+result = []
+
+for x in range(part1):
+    result.append(s1[x])
+    result.append(s2[x])
+
+for x in range(part2):
+    result.append(s3[x])
+    result.append(s4[x])
+
+random.shuffle(result)
+
+password = "".join(result)
+print("A sua nova password é: ", password)
---
+++ 03 - Anagramas/main.py
@@ -0,0 +1,30 @@
+from math import factorial
+from collections import Counter
+import itertools
+
+def count_anagrams(word):
+    n = len(word)
+    counts = Counter(word)
+
+    denominator = 1
+    for count in counts.values():
+        denominator *= factorial(count)
+
+    return factorial(n) // denominator
+
+def generate_anagrams(word):
+    return sorted(set("".join(p) for p in itertools.permutations(word)))
+
+word = input("Escreve uma palavra: ").upper()
+
+total = count_anagrams(word)
+anagrams = generate_anagrams(word)
+
+print(f"\nTotal de anagrams: {total}")
+verification_of_printanagrams = input("Quer que dê a lista de anagramas possíveis? (s/N)")
+if verification_of_printanagrams == "s":
+    print("\nLista de anagramas:")
+    for a in anagrams:
+        print(a)
+else:
+    pass
